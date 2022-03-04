cca986ddbe9bfec18c3d27f62deb46568f5f135f
ra237 <s_kurka@web.de>
2022-03-04 08:27:38 -0500

    init

---
+++ deps.ts
@@ -0,0 +1,4 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts"
+export { CommandClient, Command, Role, Extension } from "https://deno.land/x/harmony@v2.5.1/mod.ts"
+export type { ContentArgument } from "https://deno.land/x/harmony@v2.5.1/mod.ts";
+export * from "https://deno.land/x/soxa@1.4/mod.ts"
