ee9a023027065fc9ece159581c5344a0523ceda3
ra237 <s_kurka@web.de>
2022-03-05 06:55:20 -0500

    added init tests

---
+++ test.ts
@@ -0,0 +1,18 @@
+import { soxa } from "./deps.ts";
+import { assert, assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts"
+
+const TWITCH_CLIENT_ID = Deno.env.get("TWITCH_CLIENT_ID")
+const TWITCH_AUTH_TOKEN = Deno.env.get("TWITCH_AUTH_TOKEN")
+const API_BASE_URL = "https://api.twitch.tv/helix/"
+
+Deno.test("check if env variables are loaded correctly", () => {
+    assert(typeof TWITCH_CLIENT_ID === 'string', `TWITCH_CLIENT_ID type is ${typeof TWITCH_CLIENT_ID} instead of string.`)
+    assert(typeof TWITCH_AUTH_TOKEN === 'string', `TWITCH_AUTH_TOKEN type is ${typeof TWITCH_AUTH_TOKEN} instead of string.`)
+});
+
+Deno.test("check API access", async (): Promise<void> => {  
+    const url = API_BASE_URL + "search/channels?query=x";
+    const headers = { headers: { "Authorization": "Bearer " + TWITCH_AUTH_TOKEN, "Client-Id": TWITCH_CLIENT_ID } }
+    const req = await soxa.get(url, headers)
+    assertEquals(req.status, 200)
+});
