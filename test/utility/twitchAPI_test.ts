f747de46565adeb6dedd010710d8b99654b50f11
ra237 <s_kurka@web.de>
2022-03-11 11:13:41 -0500

    added more & new tests #11

---
+++ test/utility/twitchAPI_test.ts
@@ -0,0 +1,32 @@
+// deno-lint-ignore-file no-explicit-any
+import { buildUrl, searchChannel, searchStreams, API_BASE_URL, QUERIES } from "../../src/utility/twitchAPI.ts"
+import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts"
+
+Deno.test("buildUrl", () => {
+    assertEquals(buildUrl(QUERIES.search.channel), API_BASE_URL + QUERIES.search.channel)
+});
+
+Deno.test("searchChannelEmptyName", async () => {
+    const channel = await searchChannel("")
+    assertEquals(channel, undefined)
+});
+
+Deno.test("searchChannelValidName", async () => {
+    const channel: any = await searchChannel("Twitch")
+    assertEquals(channel.display_name, "Twitch")
+});
+
+Deno.test("searchChannelInvalidName", async () => {
+    const channel: any = await searchChannel("_")
+    assertEquals(channel, undefined)
+});
+
+Deno.test("streamsEmptyIds", async () => {
+    const streams = await searchStreams([])
+    assertEquals(streams, [])
+});
+
+Deno.test("streamsInvalidIds", async () => {
+    const streams = await searchStreams(["1","2","3"])
+    assertEquals(streams, [])
+});
--- test/watchStreamer_test.ts
+++ test/watchStreamer_test.ts
@@ -1,20 +1,44 @@
 // deno-lint-ignore-file no-explicit-any
 import { WatchStreamer } from "../src/commands/watchStreamer.ts"
-import { assertEquals, assertThrows } from "https://deno.land/std@0.128.0/testing/asserts.ts"
+import { assertEquals, assertThrows, assert } from "https://deno.land/std@0.128.0/testing/asserts.ts"
+
+const MOCK_CLIENT: any = {}
+
+function newWatchStreamer(): WatchStreamer {
+    const watchStreamer = new WatchStreamer(MOCK_CLIENT)
+    clearInterval(watchStreamer.interval)
+    return watchStreamer
+}
+Deno.test("watchStreamerInvalidName", async () => {
+    const watchStreamer = newWatchStreamer()
+    let reply = ""
+    const ctx: any = { message: { reply: function(text: string) { reply = text } } }
+    await watchStreamer["watchStreamer"](ctx, "_")
+    assertEquals(reply, "Streamer not found!")
+});
+
+Deno.test("watchStreamerValidName", async () => {
+    const watchStreamer = newWatchStreamer()
+    const ctx: any = { 
+        message: { reply: function() { } },
+        guild: { id: "1", roles: { fetchAll: function() { return [{ name: "twitch", id: "xyz", addTo: () => {} }] } } }
+    }
+    await watchStreamer["watchStreamer"](ctx, "twitch")
+    const isCached = watchStreamer["checkStreamerCached"]("1", "twitch")
+    assertEquals(isCached, true)
+});
 
 Deno.test("watchStreamerCached",  async () => {
-    const watchStreamer = new WatchStreamer()
+    const watchStreamer = newWatchStreamer()
     const ctx_emptyRole: any = { 
-        guild: 
-            { 
-                roles: 
-                    { 
-                        fetchAll: 
-                            function() {
-                                return new Promise<any>(res => { res([]) })
-                            } 
+        guild: { 
+            roles: { 
+                fetchAll: 
+                    function() {
+                        return new Promise<any>(res => { res([]) })
                     } 
-            }
+            } 
+        }
     }
     try {
         await watchStreamer["watchStreamerCached"](ctx_emptyRole, "epicStreamer")
@@ -24,49 +48,46 @@ Deno.test("watchStreamerCached",  async () => {
 
     let messageAuthor = ""
     const ctx_withRole: any = { 
-        guild: 
-            { 
-                roles: 
-                    { 
-                        fetchAll: 
-                            function() {
-                                return new Promise<any>(res => { res([ { name: "epicStreamer", addTo: function(usr: string) { messageAuthor = usr } } ]) })
-                            } 
+        guild: { 
+            roles: { 
+                fetchAll: 
+                    function() {
+                        return new Promise<any>(res => { res([ { name: "epicStreamer", addTo: function(usr: string) { messageAuthor = usr } } ]) })
                     } 
-            },
-        message:
-            {
-                author: "epicDiscordUser",
-                reply: function() {}
-            }
+            } 
+        },
+        message: {
+            author: "epicDiscordUser",
+            reply: function() {}
+        }
     }
     await watchStreamer["watchStreamerCached"](ctx_withRole, "epicStreamer")
     assertEquals(messageAuthor, "epicDiscordUser")
 });
 
 Deno.test("addToCache", () => {
-    const watchStreamer = new WatchStreamer()
-    watchStreamer["addToCache"]("realGuildId", "epicStreamer", "123abc")
-    assertEquals(watchStreamer["cache"]["realGuildId"], { "epicStreamer": "123abc" })
+    const watchStreamer = newWatchStreamer()
+    watchStreamer["addToCache"]("realGuildId", "epicStreamer", "iAmEpicStreamersId","123abc")
+    assert("realGuildId" in watchStreamer.cache)
+    assert("epicStreamer" in watchStreamer.cache["realGuildId"])
+    assertEquals(watchStreamer.cache["realGuildId"]["epicStreamer"].streamerId, "iAmEpicStreamersId")
+    assertEquals(watchStreamer.cache["realGuildId"]["epicStreamer"].roleId, "123abc")
+    assertEquals(watchStreamer.cache["realGuildId"]["epicStreamer"].is_live, false)
+    assert(watchStreamer.cache["realGuildId"]["epicStreamer"].nextCheck <= Date.now())
 });
 
 Deno.test("checkStreamerCached", () => {
-    const watchStreamer = new WatchStreamer()
+    const watchStreamer = newWatchStreamer()
     assertEquals(watchStreamer["checkStreamerCached"]("abc", "abc"), false)
 
-    watchStreamer["cache"] = { "realGuildId": { "epicStreamer": "123abc"} }
+    watchStreamer["addToCache"]("realGuildId", "epicStreamer", "iAmEpicStreamersId","123abc")
     assertEquals(watchStreamer["checkStreamerCached"]("realGuildId", "epicStreamer"), true)
 });
 
 Deno.test("isGuildCached", () => {
-    const watchStreamer = new WatchStreamer()
+    const watchStreamer = newWatchStreamer()
     assertEquals(watchStreamer["isGuildCached"]("abc"), false)
     
     watchStreamer["cache"] = { "realGuildId": {} }
     assertEquals(watchStreamer["isGuildCached"]("realGuildId"), true)
-});
-
-Deno.test("toString", () => {
-    const watchStreamer = new WatchStreamer()
-    assertEquals(watchStreamer.toString(), watchStreamer.name)
 });
