03bb5550fc7249e806f0ab3ef885bb22381f305b
ra237 <s_kurka@web.de>
2022-03-11 18:59:24 -0500

    added Embed

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts"
-export { CommandClient, Command, Role, Extension } from "https://deno.land/x/harmony@v2.5.1/mod.ts"
+export { CommandClient, Command, Role, Extension, Embed } from "https://deno.land/x/harmony@v2.5.1/mod.ts"
 export type { ContentArgument, Guild, GuildChannels, TextChannel, Collection } from "https://deno.land/x/harmony@v2.5.1/mod.ts";
 export * from "https://deno.land/x/soxa@1.4/mod.ts"
