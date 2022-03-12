7f779d86f2fa6a3fdb57b5475370a8576204c7a3
ra237 <s_kurka@web.de>
2022-03-11 19:00:15 -0500

    added static method to get all sub commands

--- mod.ts
+++ mod.ts
@@ -1,13 +1,16 @@
 import { CommandClient, Command, Extension } from "./deps.ts"
 import { CommandContext } from "https://deno.land/x/harmony@v2.5.1/mod.ts"
 import { WatchStreamer } from "./src/commands/watchStreamer.ts"
+import { Help } from "./src/commands/help.ts"
 import { generateUsageSubCommands } from "./src/utility/commandUsage.ts"
 
 export class TwitchExtension extends Extension {
     name = "Twitch"
     constructor(client: CommandClient) {
         super(client)
-        this.commands.add(new Twitch(client))
+        const tw = new Twitch(client)
+        this.commands.add(tw)
+        Twitch.allSubCommands = tw.getSubCommands()
         console.log(`\t+ ${this.name} Extension loaded.`)
     }
 }
@@ -18,16 +21,21 @@ export class Twitch extends Command {
     usage = "**USAGE**: !twitch "
     description = "Base command for all twitch utility the bot provides."
     client: CommandClient
+    static allSubCommands: Command[]
 
     constructor(client: CommandClient) {
         super()
         this.client = client
-        this.subCommands = [ new WatchStreamer(client) ]
+        this.subCommands = [ new Help(), new WatchStreamer(client) ]
     }
     
     // this class will only execute when no valid sub-command is given
     execute(ctx: CommandContext): void {
         const subUsage = generateUsageSubCommands(this.getSubCommands())
         ctx.message.reply(this.usage + subUsage)
+    }
+
+    public static getAllSubCommands(): Command[] {
+        return this.allSubCommands
     }
 }
