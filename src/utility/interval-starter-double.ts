29aac36d09e1b1001c6b8548202fda531ea35e96
ra237 <s_kurka@web.de>
2022-03-18 06:38:45 -0400

    returning impossible number for testing

--- src/utility/interval-starter-double.ts
+++ src/utility/interval-starter-double.ts
@@ -2,8 +2,8 @@ import { IntervalStarterInterface } from "./interval-starter-interface.ts";
 
 export class IntervalStarterDouble implements IntervalStarterInterface {
 
-    public startInterval(lengthOfIntervalInSeconds: number): number {
-        return 0 // do nothing else I'm just a double
+    public startInterval(_lengthOfIntervalInSeconds: number): number {
+        return -1
     }
 
 }
