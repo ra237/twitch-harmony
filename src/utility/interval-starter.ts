81a9374366f7fcfa124ad32a65526b6baa87e9d0
ra237 <s_kurka@web.de>
2022-03-18 06:39:30 -0400

    added missing implementation

--- src/utility/interval-starter.ts
+++ src/utility/interval-starter.ts
@@ -1,10 +1,10 @@
+import { WatchStreamer } from "../commands/watchStreamer.ts";
 import { IntervalStarterInterface } from "./interval-starter-interface.ts";
 
 export class IntervalStarter implements IntervalStarterInterface {
 
-    public startInterval(lengthOfIntervalInSeconds: number): number {
-        return 0
-        // return setInterval(() => this.isStreamerLive(), lengthOfIntervalInSeconds * 1000)
+    public startInterval(lengthOfIntervalInSeconds: number, watchStreamer: WatchStreamer): number {
+        return setInterval(() => watchStreamer.checkStreamerLive(), lengthOfIntervalInSeconds * 1000)
     }
 
 }
